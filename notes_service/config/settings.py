b75e854de600ce67c33f30d178f1d88691d4214f
akhil0699 <67257818+akhil0699@users.noreply.github.com>
2025-08-21 19:10:47 +0530

    Update settings.py

--- notes_service/config/settings.py
+++ notes_service/config/settings.py
@@ -5,11 +5,11 @@ from pydantic_settings import BaseSettings
 
 
 class AppConfig(BaseSettings):
-    DATABASE_URL: str
+    DATABASE_URL: str = "mysql+pymysql://avnadmin:AVNS_-3oMhPUnpnpqZeh0-le@mysql-a35ba9b-akhilinthehome6-24a4.f.aivencloud.com:16017/notes_akhil"
     ALLOW_ORIGINS: str = "*"
     ALLOW_CREDENTIALS: bool = True
-    APP_URL: str = "http://localhost:8001"
-    DATABASE_CA_CERT_FILE_PATH: str
+    APP_URL: str = "https://my-notes-backend-t5xa.onrender.com"
+    DATABASE_CA_CERT_FILE_PATH: str 
 
     class Config:
         env_file = ".env"
